bd27cd604de89a89702b63bb80ed7556da51a0a0
jhtonel <jhtonel@gmail.com>
2024-01-15 00:08:48 -0300

    first commit

---
+++ src/Pages/Home/Index.jsx
@@ -0,0 +1,316 @@
+import "./Styles.scss";
+
+export function Home() {
+  return (
+    <>
+      <div id="background">
+        <div className="stars">
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+        </div>
+        <svg id="bg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+          <defs>
+            <radialGradient
+              id="Gradient1"
+              cx="50%"
+              cy="50%"
+              fx="0.441602%"
+              fy="50%"
+              r=".5"
+            >
+              <animate
+                attributeName="fx"
+                dur="34s"
+                values="0%;3%;0%"
+                repeatCount="indefinite"
+              ></animate>
+              <stop offset="0%" stop-color="rgba(26,27,90, 1)"></stop>
+              <stop offset="100%" stop-color="rgba(26,17,90, 0)"></stop>
+            </radialGradient>
+            <radialGradient
+              id="Gradient2"
+              cx="50%"
+              cy="50%"
+              fx="2.68147%"
+              fy="50%"
+              r=".5"
+            >
+              <animate
+                attributeName="fx"
+                dur="23.5s"
+                values="0%;3%;0%"
+                repeatCount="indefinite"
+              ></animate>
+              <stop offset="0%" stop-color="rgba(33,19,105)"></stop>
+              <stop offset="100%" stop-color="rgba(33,19,105, 0)"></stop>
+            </radialGradient>
+            <radialGradient
+              id="Gradient3"
+              cx="50%"
+              cy="50%"
+              fx="0.836536%"
+              fy="50%"
+              r=".5"
+            >
+              <animate
+                attributeName="fx"
+                dur="21.5s"
+                values="0%;3%;0%"
+                repeatCount="indefinite"
+              ></animate>
+              <stop offset="0%" stop-color="rgba(26,27,119, 1)"></stop>
+              <stop offset="100%" stop-color="rgba(26,27,119, 0)"></stop>
+            </radialGradient>
+            <radialGradient
+              id="Gradient4"
+              cx="50%"
+              cy="50%"
+              fx="4.56417%"
+              fy="50%"
+              r=".5"
+            >
+              <animate
+                attributeName="fx"
+                dur="23s"
+                values="0%;5%;0%"
+                repeatCount="indefinite"
+              ></animate>
+              <stop offset="0%" stop-color="rgba(39,39,132, 1)"></stop>
+              <stop offset="100%" stop-color="rgba(39,39,132 0)"></stop>
+            </radialGradient>
+            <radialGradient
+              id="Gradient5"
+              cx="50%"
+              cy="50%"
+              fx="2.65405%"
+              fy="50%"
+              r=".5"
+            >
+              <animate
+                attributeName="fx"
+                dur="24.5s"
+                values="0%;5%;0%"
+                repeatCount="indefinite"
+              ></animate>
+              <stop offset="0%" stop-color="rgba(25,25,133, 1)"></stop>
+              <stop offset="100%" stop-color="rgba(25,25,133, 0)"></stop>
+            </radialGradient>
+          </defs>
+          <rect
+            x="13.744%"
+            y="1.18473%"
+            width="100%"
+            height="100%"
+            fill="url(#Gradient1)"
+            transform="rotate(334.41 50 50)"
+          >
+            <animate
+              attributeName="x"
+              dur="20s"
+              values="25%;0%;25%"
+              repeatCount="indefinite"
+            ></animate>
+            <animate
+              attributeName="y"
+              dur="21s"
+              values="0%;25%;0%"
+              repeatCount="indefinite"
+            ></animate>
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 50 50"
+              to="360 50 50"
+              dur="7s"
+              repeatCount="indefinite"
+            ></animateTransform>
+          </rect>
+          <rect
+            x="-2.17916%"
+            y="35.4267%"
+            width="100%"
+            height="100%"
+            fill="url(#Gradient2)"
+            transform="rotate(255.072 50 50)"
+          >
+            <animate
+              attributeName="x"
+              dur="23s"
+              values="-25%;0%;-25%"
+              repeatCount="indefinite"
+            ></animate>
+            <animate
+              attributeName="y"
+              dur="24s"
+              values="0%;50%;0%"
+              repeatCount="indefinite"
+            ></animate>
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 50 50"
+              to="360 50 50"
+              dur="12s"
+              repeatCount="indefinite"
+            ></animateTransform>
+          </rect>
+          <rect
+            x="9.00483%"
+            y="14.5733%"
+            width="100%"
+            height="100%"
+            fill="url(#Gradient3)"
+            transform="rotate(139.903 50 50)"
+          >
+            <animate
+              attributeName="x"
+              dur="25s"
+              values="0%;25%;0%"
+              repeatCount="indefinite"
+            ></animate>
+            <animate
+              attributeName="y"
+              dur="12s"
+              values="0%;25%;0%"
+              repeatCount="indefinite"
+            ></animate>
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="360 50 50"
+              to="0 50 50"
+              dur="9s"
+              repeatCount="indefinite"
+            ></animateTransform>
+          </rect>
+        </svg>
+      </div>
+      <div className="Home">
+        <div id="container">
+          <div id="menu">
+            <div id="profile">
+              <img />
+              <p id="name">José Henrique Oliveira</p>
+            </div>
+            <div id="switch" onclick="toggleMode()">
+              <button></button>
+              <span></span>
+            </div>
+            <nav id="list">
+              <ul>
+                <li id="certificades" onclick="openMenu()">
+                  <button>
+                    <span>Certificados</span>
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    <span>Certificados</span>
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    <span>Certificados</span>
+                  </button>
+                </li>
+              </ul>
+            </nav>
+            <div id="social_links" className="social-media-buttons">
+              <a
+                href="https://github.com/Jhtonel"
+                target="_blank"
+                rel="noreferrer"
+                className="social-media-button"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="65%"
+                  viewBox="0 0 496 512"
+                >
+                  <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 70.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                </svg>
+              </a>
+              <a
+                href="https://linkedin.com/in/jose-tonel"
+                target="_blank"
+                rel="noreferrer"
+                className="social-media-button"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="65%"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" />
+                </svg>
+              </a>
+              <a
+                href="https://wa.me//5512996878788"
+                target="_blank"
+                rel="noreferrer"
+                className="social-media-button"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="65%"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <script src="script.js"></script>
+      </div>
+    </>
+  );
+}
